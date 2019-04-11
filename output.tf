2fd582b87464868e98bb6ac207dd59a4a790489a
adekunle ibitayo <adekunle.ibitayo@appvia.io>
2019-04-11 23:22:51 +0100

    adding guard duty output key Id

---
+++ output.tf
@@ -0,0 +1,69 @@
+output "eu_west_2_id" {
+  description = "The guard Dector ID  created"
+  value       = "${element(concat(aws_guardduty_detector.eu_west_2.*.id, list("")), 0)}"
+}
+
+output "eu_west_1_id" {
+  description = "The guard Dector ID  created"
+  value       = "${element(concat(aws_guardduty_detector.eu_west_1.*.id, list("")), 0)}"
+}
+
+output "eu_west_3_id" {
+  description = "The guard Dector ID  created"
+  value       = "${element(concat(aws_guardduty_detector.eu_west_3.*.id, list("")), 0)}"
+}
+
+output "sa_east_1_id" {
+  description = "The guard Dector ID  created"
+  value       = "${element(concat(aws_guardduty_detector.sa_east_1.*.id, list("")), 0)}"
+}
+
+output "eu_central_1_id" {
+  description = "The guard Dector ID  created"
+  value       = "${element(concat(aws_guardduty_detector.eu_central_1.*.id, list("")), 0)}"
+}
+
+output "ap_south_1_id" {
+  description = "The guard Dector ID  created"
+  value       = "${element(concat(aws_guardduty_detector.ap_south_1.*.id, list("")), 0)}"
+}
+
+output "north_east_2_id" {
+  description = "The guard Dector ID  created"
+  value       = "${element(concat(aws_guardduty_detector.north_east_2.*.id, list("")), 0)}"
+}
+
+output "north_east_1_id" {
+  description = "The guard Dector ID  created"
+  value       = "${element(concat(aws_guardduty_detector.north_east_1.*.id, list("")), 0)}"
+}
+
+output "south_east_2_id" {
+  description = "The guard Dector ID  created"
+  value       = "${element(concat(aws_guardduty_detector.south_east_2.*.id, list("")), 0)}"
+}
+
+output "south_east_1_id" {
+  description = "The guard Dector ID  created"
+  value       = "${element(concat(aws_guardduty_detector.south_east_1.*.id, list("")), 0)}"
+}
+
+output "us_east_1_id" {
+  description = "The guard Dector ID  created"
+  value       = "${element(concat(aws_guardduty_detector.us_east_1.*.id, list("")), 0)}"
+}
+
+output "us_east_2_id" {
+  description = "The guard Dector ID  created"
+  value       = "${element(concat(aws_guardduty_detector.us_east_2.*.id, list("")), 0)}"
+}
+
+output "us_west_1_id" {
+  description = "The guard Dector ID  created"
+  value       = "${element(concat(aws_guardduty_detector.us_west_1.*.id, list("")), 0)}"
+}
+
+output "us_west_2_id" {
+  description = "The guard Dector ID  created"
+  value       = "${element(concat(aws_guardduty_detector.us_west_2.*.id, list("")), 0)}"
+}
